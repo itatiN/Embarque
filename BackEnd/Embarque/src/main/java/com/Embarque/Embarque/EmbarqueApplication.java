dfe8c950f16c26164313eec95e4c995fe870e60a
kyosho-dev <gabrielfaluga@hotmail.com>
2024-04-18 11:21:20 -0300

    add boiler-plate

---
+++ BackEnd/Embarque/src/main/java/com/Embarque/Embarque/EmbarqueApplication.java
@@ -0,0 +1,13 @@
+package com.Embarque.Embarque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbarqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbarqueApplication.class, args);
+	}
+
+}
---
+++ BackEnd/Embarque/src/test/java/com/Embarque/Embarque/EmbarqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Embarque.Embarque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbarqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
