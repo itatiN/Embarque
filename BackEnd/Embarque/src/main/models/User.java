b49920a65af53e92682f5913fcbc03764049de4f
itati <itaticontato@gmail.com>
2024-05-19 19:48:11 -0300

    add: User DTO

---
+++ BackEnd/Embarque/src/main/models/User.java
@@ -0,0 +1,33 @@
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+// Aconselho sempre nomear as tabelas em minusculo
+// Isso faz ocupar menos memoria e gasta menos energia
+// É pouco o que salvamos de energia, mas é algo.
+public class User {
+    @Id
+    @GeneratedValue(strategy = "uuid2")
+    @GeneratedValue(strategy = "uuid2")
+    @Column(name = "id", columnDefinition = "BINARY(16)")
+    private UUID id;
+    // Preferi escolher a definicao de coluna para o tipo BINARY(16)
+    // pois isso minimiza o espaco(menos bytes e sem caracteres especiais)
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
