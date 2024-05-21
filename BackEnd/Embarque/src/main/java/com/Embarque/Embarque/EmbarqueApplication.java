31db1b181ccad2094c2e2c1e1301c91b812fd0fc
kyosho-dev <gabrielfaluga@hotmail.com>
2024-05-20 22:33:28 -0300

    Create: Permissoes

--- BackEnd/Embarque/src/main/java/com/Embarque/Embarque/EmbarqueApplication.java
+++ BackEnd/Embarque/src/main/java/com/Embarque/Embarque/EmbarqueApplication.java
@@ -2,7 +2,10 @@ package com.Embarque.Embarque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class EmbarqueApplication {
 
@@ -10,4 +13,8 @@ public class EmbarqueApplication {
 		SpringApplication.run(EmbarqueApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public static String hola(){
+		return "Hola";
+	}
 }
---
+++ BackEnd/Embarque/src/main/models/Permissoes.java
@@ -0,0 +1,26 @@
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "permissoes")
+public class Permissoes {
+    @Id
+    @GeneratedValue(strategy = "uuid2")
+    @Column(name = "id", columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    // a anotação OneToOne junto com a anotação JoinColumn indica que o atributo user é uma chave estrangeira
+    @OneToOne 
+    @JoinColumn(name = "fk_userId", nullable=false)
+    private User user;
+
+    @Column(name = "permissao", nullable=false)
+    private String permissao;
+}
