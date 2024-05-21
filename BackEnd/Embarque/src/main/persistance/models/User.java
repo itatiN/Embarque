735f913dba7979c6c6aef932e044afba227ee1ba
itati <itaticontato@gmail.com>
2024-05-21 14:57:18 -0300

    add: Permissoes DTO

--- BackEnd/Embarque/src/main/persistance/models/User.java
+++ BackEnd/Embarque/src/main/persistance/models/User.java
@@ -1,7 +1,11 @@
 import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.SecondaryTable;
+import jakarta.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +14,7 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name = "user")
+@SecondaryTable(name = "permissao", pkJoinColumns = @PrimaryKeyJoinColumn(name = "id"))
 // Aconselho sempre nomear as tabelas em minusculo
 // Isso faz ocupar menos memoria e gasta menos energia
 // É pouco o que salvamos de energia, mas é algo.
