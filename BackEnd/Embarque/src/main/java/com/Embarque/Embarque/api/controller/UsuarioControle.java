285a5b02137407d571548eff57a1fc62f97d17a9
kyosho-dev <gabrielfaluga@hotmail.com>
2024-05-23 08:58:34 -0300

    REST

--- BackEnd/Embarque/src/main/java/com/Embarque/Embarque/api/controller/UsuarioControle.java
+++ BackEnd/Embarque/src/main/java/com/Embarque/Embarque/api/controller/UsuarioControle.java
@@ -1,30 +1,23 @@
-package com.Embarque.Embarque.api.services;
+package com.Embarque.Embarque.api.controller;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.Embarque.Embarque.models.User;
+import com.Embarque.Embarque.api.services.UserService;
+import com.Embarque.Embarque.models.Cliente;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.integration.IntegrationProperties.RSocket.Client;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-import java.util.Optional;
-
-
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class UsuarioControle{
-
 	@Autowired
-    private IU SQL;
+    private UserService UserService;
 
     @GetMapping
 	public static String hola(){
@@ -32,9 +25,8 @@ public class UsuarioControle{
 	}
 
 	@PostMapping
-	public Client criarUser(@RequestBody Client user) {
-		Client newUser = SQL.save(user);
-		return newUser;
+	public Cliente criarcClient(@RequestBody Cliente cliente) {
+		return UserService.saveCliente(cliente);
 	}
 
 	@DeleteMapping("/{ID_user}")
