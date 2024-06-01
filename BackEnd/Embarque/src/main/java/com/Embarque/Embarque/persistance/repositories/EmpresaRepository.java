5d74d8f61b46c6795a2fe0c0fa5985316fdb57fd
Jadir <jadir.par@gmail.com>
2024-06-01 14:37:30 -0300

    services e controllers para empresa

--- BackEnd/Embarque/src/main/java/com/Embarque/Embarque/persistance/repositories/EmpresaRepository.java
+++ BackEnd/Embarque/src/main/java/com/Embarque/Embarque/persistance/repositories/EmpresaRepository.java
@@ -1,10 +1,13 @@
 package com.Embarque.Embarque.persistance.repositories;
+
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.Embarque.Embarque.persistance.models.Empresa;
 
 @Repository
-public  interface EmpresaRepository extends CrudRepository<Empresa,Integer> {
-    
+public interface EmpresaRepository extends CrudRepository<Empresa, Integer> {
+    Optional<Empresa> findById(Long id);
 }
